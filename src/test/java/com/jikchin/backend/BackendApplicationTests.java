1ffe09916e95f2dedca6dc77720d88ea92ee5a6d
RyuJungKyun <prank01@naver.com>
2026-08-27 12:35:09 +0900

    chore: 프로젝트 초기 셋팅

---
+++ src/test/java/com/jikchin/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikchin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
