082903dcd925fa8c7fa67edbf3900057b2fc7d56
RyuJungKyun <prank01@naver.com>
2026-09-11 16:37:14 +0900

    chore: 인덱스 및 SQL 로그 설정 예시 주석 보관

--- src/main/java/com/jikchin/jikchinbackend/domain/event/entity/Event.java
+++ src/main/java/com/jikchin/jikchinbackend/domain/event/entity/Event.java
@@ -10,7 +10,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "events")
+@Table(
+    name = "events"
+    /*
+       indexes = {
+         @Index(name = "idx_events_starts_at", columnList = "starts_at"),
+         @Index(name = "idx_events_sport_starts_at", columnList = "sport_id,starts_at")
+       }
+
+    */
+    )
 public class Event extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
